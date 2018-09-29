8a6659d5ea143bff09877a20590448287721d2c7
Roman Eckert <mail@romaneckert.de>
2018-09-29 19:14:26 +0200

    clean class definition

--- app/class-definition.js
+++ app/class-definition.js
@@ -1,17 +1,10 @@
 class ClassDefinition {
 
     constructor(type, name) {
-        this._type = ('string' === typeof type) ? type.toLowerCase() : 'undefined';
-        this._name = ('string' === typeof name) ? name.toLowerCase() : 'undefined';
+        this.type = ('string' === typeof type) ? type.toLowerCase() : 'undefined';
+        this.name = ('string' === typeof name) ? name.toLowerCase() : 'undefined';
     }
 
-    get type() {
-        return this._type;
-    }
-
-    get name() {
-        return this._name;
-    }
 }
 
 module.exports = ClassDefinition;
