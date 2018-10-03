a68d2c05f9fe81287f9d2b1fe3a8dd3eb84874a0
Roman Eckert <mail@romaneckert.de>
2018-10-03 21:16:32 +0200

    optimize core

--- index.js
+++ index.js
@@ -63,6 +63,11 @@ class Core {
             }
         }
 
+        // log informations about start process of core
+        if (cluster.worker && cluster.worker.id === 1) {
+            this.module.logger.log('application run in env: ' + process.env.NODE_ENV, '', this._classDefinition, undefined, 6);
+        }
+
         // check ready state modules
         this._startCheckModules();
 
@@ -76,9 +81,6 @@ class Core {
         } else {
             this.module.server.start();
         }
-
-        // TODO: only in main cluster process
-        this.module.logger.log('application run in env: ' + process.env.NODE_ENV, '', this._classDefinition, undefined, 6);
 
     }
 
