d8165005146a6e97cb649b24c6eec73c066f5b91
Roman Eckert <mail@romaneckert.de>
2018-10-04 22:18:32 +0200

    change log message error middleware

--- app/middleware/error.js
+++ app/middleware/error.js
@@ -3,7 +3,7 @@ const AbstractMiddleware = require('../core/abstract-middleware');
 class Error extends AbstractMiddleware {
 
     handle(err, req, res, next) {
-        this.logger.error('error in "' + req.url + '" with message: "' + err.message + '"', null, this.util.error.stack(err));
+        this.logger.error('error in "' + req.url + '" with message: ' + err.message, null, this.util.error.stack(err));
         return res.render('core/middleware/error');
     }
 
