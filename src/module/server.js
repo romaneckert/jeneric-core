e416ad4c1c24999079d85bc722ec0626b8d00b1b
Roman Eckert <mail@romaneckert.de>
2018-10-17 22:11:26 +0200

    add test string type in add routes server

--- src/module/server.js
+++ src/module/server.js
@@ -33,7 +33,7 @@ class Server {
         for (let routeName in routes) {
             let route = routes[routeName];
 
-            if ('string' === typeof route.path) {
+            if ('string' === typeof route.path && 'string' === typeof route.handler) {
                 let handler = this._getHandlerFromHandlerString(route.handler);
 
                 express[route.method](route.path, handler.handle.bind(handler));
