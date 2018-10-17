dfc2e001d39d228f3ae1f286e3b6c73480fcd5a1
Roman Eckert <mail@romaneckert.de>
2018-10-17 20:53:31 +0200

    fix middleware error message

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -3,7 +3,7 @@ const errorUtil = require('../../util/error');
 class Error {
 
     handle(err, req, res, next) {
-        this.logger.error(req.url + ' ' + err.message + '"', null, errorUtil.stack(err));
+        this.logger.error(req.url + ' ' + err.message, null, errorUtil.stack(err));
         return res.render('core/middleware/error');
     }
 
