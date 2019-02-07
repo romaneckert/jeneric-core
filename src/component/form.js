3d696bf8c73c59bf72d928caa0c32871f8ed9766
Roman Eckert <mail@romaneckert.de>
2019-02-07 21:06:12 +0100

    work on form component

--- src/component/form.js
+++ src/component/form.js
@@ -2,14 +2,22 @@ const mongoose = require('mongoose');
 const objectUtil = require('../../util/object');
 
 class Form {
-    constructor(instance) {
+    constructor(schema, instance) {
+
+        this.schema = schema;
         this.instance = instance;
         this.errors = {};
         this.submitted = false;
-        this.valid = true;
+        this.valid = false;
+
+        if (null === this.schema && null === this.instance) {
+            throw new Error('schema and instance are null');
+        }
     }
 
     handle(data) {
+
+        this.errors = {};
 
         if (0 === Object.keys(data).length) {
             return this;
@@ -17,22 +25,34 @@ class Form {
 
         this.submitted = true;
 
-        objectUtil.merge(this.instance, data);
+        let instance = null;
 
-        let errors = this.instance.validateSync();
+        if (null !== this.schema) {
+            let Model = mongoose.model('JenericForm', new mongoose.Schema(this.schema));
+            instance = new Model(data);
+        } else {
+            instance = this.instance;
+            objectUtil.merge(instance, data);
+        }
+
+        let errors = instance.validateSync();
+
+        mongoose.deleteModel('JenericForm');
 
         if (undefined === errors) {
             this.valid = true;
         } else {
-            this.errors = errors.errors;
+            for (let key in errors.errors) {
+                this.errors[key] = errors.errors[key].message;
+            }
         }
 
         return this;
     }
 
-    addError(error) {
+    addError(key, message) {
 
-        objectUtil.merge(this.errors, error);
+        this.errors[key] = message;
 
         return this;
     }
