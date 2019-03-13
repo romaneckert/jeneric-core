05ac27575a84ac226377af6e1d8d899b7024ba1a
Roman Eckert <mail@romaneckert.de>
2019-03-13 20:12:31 +0100

    add some comments to asset module

--- src/module/asset.js
+++ src/module/asset.js
@@ -4,24 +4,24 @@ const fs = require('../util/fs');
 class Asset {
 
     constructor(config) {
-
         this.config = config;
-
         this.pathToPublic = null;
     }
 
     init() {
+        // set path to public folder of the current project
         this.pathToPublic = path.join(this.container.config.directories.slice(-1).pop(), 'public');
     }
 
-    asset(src, opt) {
+    asset(src, options) {
 
-        if ('object' !== typeof opt) opt = {};
+        // init options object
+        if ('object' !== typeof options) options = {};
 
-        if (opt.source) {
-            return fs.readFileSync(path.join(this.pathToPublic, src));
-        }
+        // if options.source is true, then return content of file
+        if (options.source) return fs.readFileSync(path.join(this.pathToPublic, src));
 
+        // return path to file in asset folder with timestamp of server start date / do not check if file exists becaus eof performance
         return path.join('/assets/', src) + '?' + Math.floor(this.container.env.startDate.getTime() / 1000);
     }
 
